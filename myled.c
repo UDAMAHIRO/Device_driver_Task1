8dc208bda1977467446b7c9bf9186f664f146ff3
Mahiro Uda <s19c1017as@s.chibakoudai.jp>
2020-12-21 17:53:42 +0000

    Add a file

---
+++ myled.c
@@ -0,0 +1,100 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/uaccess.h>
+#include <linux/io.h>
+
+MODULE_AUTHOR("Ryuichi Ueda and Mahiro Uda");
+MODULE_DESCRIPTION("driver for LED control");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.0.1");
+
+static dev_t dev;
+static struct cdev cdv;
+static struct class *cls = NULL;
+static volatile u32 *gpio_base = NULL;
+static int leds[4] = {22, 23, 24, 25};
+
+static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
+{
+	int i;
+	char c;
+	if(copy_from_user(&c, buf, sizeof(char)))
+		return -EFAULT;
+
+//	printk(KERN_INFO "receive %c\n", c);
+	if(c == 'f')
+	{
+		for(i = 0; i < 4; i++)
+			gpio_base[10] = 1 << leds[i];
+	}
+	else if(c == 'n')
+	{
+		for(i = 0; i < 4; i++)
+			gpio_base[7] = 1 << leds[i];
+	}
+	else if(c == 'p')
+	{	for(i = 0; i < 3; i++)
+			gpio_base[7] = 1 << leds[i];
+		gpio_base[10] = 1 << leds[i];
+	}
+
+	return 1;
+}
+
+static struct file_operations led_fops = {
+	.owner = THIS_MODULE,
+	.write = led_write,
+	.read = sushi_read
+};
+
+static int __init init_mod(void)
+{
+	int retval;
+	int i;
+	retval = alloc_chrdev_region(&dev, 0, 1, "myled");
+	if(retval < 0){
+		printk(KERN_ERR "alloc_chrdev_region failed.\n");
+		return retval;
+	}
+	printk(KERN_INFO "%s is loaded. major:%d\n", __FILE__, MAJOR(dev));
+
+	cdev_init(&cdv, &led_fops);
+	retval = cdev_add(&cdv, dev, 1);
+	if(retval < 0){
+		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d", MAJOR(dev), MINOR(dev));
+		return retval;
+	}
+	cls = class_create(THIS_MODULE, "myled");
+	if(IS_ERR(cls)){
+		printk(KERN_ERR "class_create failed.");
+		return PTR_ERR(cls);
+	}
+	device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));
+
+	gpio_base = ioremap_nocache(0xfe200000, 0xA0);
+
+	for(i = 0; i < 4; i++)
+	{
+		const u32 led = leds[i];
+		const u32 index = led/10;
+		const u32 shift = (led%10)*3;
+		const u32 mask = ~(0x7 << shift);
+		gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+	}
+
+	return 0;
+}
+
+static void __exit cleanup_mod(void)
+{
+	cdev_del(&cdv);
+	device_destroy(cls, dev);
+	class_destroy(cls);
+	unregister_chrdev_region(dev, 1);
+	printk(KERN_INFO "%s is unloaded. major:%d\n", __FILE__, MAJOR(dev));
+}
+
+module_init(init_mod);
+module_exit(cleanup_mod);
